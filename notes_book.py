93f088b4b6ab6533899ad2b6c2f2cdfb64cb84ac
Andr-Mih <75747892+Andr-Mih@users.noreply.github.com>
2021-04-12 00:03:58 +0300

    Add files via upload

--- notes_book.py
+++ notes_book.py
@@ -10,15 +10,19 @@ class Field():
 
 class Note(Field):
 
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
     @property
-    def note(self):
-        return self.__note 
+    def value(self):
+        return self.__value
     
-    @note.setter
-    def note(self, value):
-        if re.match(r'[\w{5}]', value) and len(value) < 30:
-            self.__note = value
-        elif re.match(r'[\w{5}]', value) and len(value) > 30:
+    @value.setter
+    def value(self, value):
+        if re.match(r'[A-Za-zА-Яа-я]\w{2}', value) and len(value) < 30:
+            self.__value = value
+        elif re.match(r'[A-Za-zА-Яа-я]\w{2}', value) and len(value) > 30:
             print('Note is more lenght')
         elif len(value) < 4:
             print('Note is too short')
@@ -28,14 +32,20 @@ class Note(Field):
 
 class Teg(Field):
 
-    @property
-    def teg(self):
-        return self.__teg
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
 
-    @teg.setter
-    def teg(self, value):
-        if re.match(r'\w{5}'):
-            self.__teg = value
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not value:
+            self.__value = value
+        if re.match(r'[A-Za-zА-Яа-я]\w{2}', value) and len(value) >= 3:
+            self.__value = value
         else:
             print(f'Teg is too long')
 
@@ -69,10 +79,53 @@ class NotesBook(UserDict):
 
     def __init__(self):
         super(NotesBook, self).__init__()
+        self._count = 0
 
     def add_record(self, obj):
         if isinstance(obj, NoteRecord):
             self.data[len(self.data)] = obj.noterecors
+            return self.data
+
+ # edit and del NOTE
+    def edit_note(self, index, new_obj):
+        self._count = 0
+        if len(self.data) > index:
+            if isinstance(new_obj, Note):
+                for i in self.data.values():
+                    if self._count == index:
+                        i['Note'] = new_obj.value
+                    self._count +=1
+            return self.data
+        else:
+            return 'Note index is not exist'
+
+    def del_note(self, index):
+        if len(self.data) > index:
+            del self.data[index]
+            return self.data
+        else:
+            return 'Note index is not exist'
+
+# edit and del TEG
+    def edit_teg(self, index, new_obj):
+        self._count = 0
+        if len(self.data) > index:
+            if isinstance(new_obj, Teg):
+                for i in self.data.values():
+                    if self._count == index:
+                        i['Teg'] = new_obj.value
+                    self._count +=1
+            return self.data
+        else:
+            return 'Teg index is not exist' 
+
+    def del_teg(self, index):
+        if len(self.data) > index:
+            self.data[index]['Teg'] = None
+            return self.data
+        else:
+            return 'Teg index is not exist'
+            
 
 
 
@@ -89,6 +142,14 @@ def load_notesBook():
     else:
         with file:
             return pickle.load(file)
+
+
+if __name__ == '__main__':
+
+    pass
+    
+
+
 
 
 
