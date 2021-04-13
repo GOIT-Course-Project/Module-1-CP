6e4b638d497b531c587e9d3303d49893c02d295d
Andr-Mih <75747892+Andr-Mih@users.noreply.github.com>
2021-04-13 22:21:13 +0300

    setup with console entry

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+name = 'personal_assistant',
+version = '1.0.0',
+authors = 'Volodymyr Dunkin', 'Andrii Mikhalkin',
+description = 'personal assistant',
+entry_points = {'console_scripts': ['assist = personal_assistant.general:main']},
+long_description = 'This application is intended for organizing, organizing and storing contacts and text notes',
+license = 'MIT license',
+
+packages = find_namespace_packages()
+)
